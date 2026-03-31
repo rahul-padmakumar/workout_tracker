0a6114d426cbd3627430473d63db671603db0e8f
rahulp <rahulp5875@gmail.com>
2026-03-31 15:24:01 +0530

    Add calculator functionality and unit tests; update docker-compose for database service

---
+++ app/app/test.py
@@ -0,0 +1,11 @@
+"""
+Unit tests for the calc module.
+"""
+from django.test import SimpleTestCase
+from .calc import add
+
+class CalcTest(SimpleTestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(0, 0), 0)
