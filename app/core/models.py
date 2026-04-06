b9011864898bd7522aa97c657ddfba901a385cb1
rahulp <rahulp5875@gmail.com>
2026-04-06 17:47:57 +0530

    Implement custom user model with email and phone number fields; update user manager and tests

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,51 @@
-from django.db import models  # noqa
+"""
+Models for the authentication system
+"""
+from django.db import models
+from django.contrib.auth.models import (
+  AbstractBaseUser,
+  BaseUserManager,
+  PermissionsMixin
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users"""
+
+    def create_user(self, email, password=None,
+                    phone_number=None, **extra_fields):
+        """Create and save a new user"""
+        if not email:
+            raise ValueError('Users must have an email address')
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            phone_number=phone_number,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and save a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['phone_number']
