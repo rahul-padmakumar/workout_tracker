f1b6b21e2c3fe08c3f1396cf5bd23ae5dc092f95
rahulp <rahulp5875@gmail.com>
2026-04-07 17:59:44 +0530

    Add test for user creation without email to raise ValueError

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -34,3 +34,8 @@ class TestModels(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'test123', '1234567890')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test creating a user without an email raises a ValueError"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123', '1234567890')
