efd9f6615ea9db6d090d0922f1a07ba7d36f1fc2
rahulp <rahulp5875@gmail.com>
2026-04-10 14:52:27 +0530

    Add UserAdmin class to manage users in the admin panel and implement corresponding tests

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin # noqa
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+
+@admin.register(models.User)
+class UserAdmin(BaseUserAdmin):
+    """Define the admin page for users"""
+    list_display = ['email', 'phone_number', 'is_active', 'is_staff']
+    ordering = ['id']
+
+
