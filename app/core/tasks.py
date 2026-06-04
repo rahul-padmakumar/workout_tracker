be0980c229454463dc28269ef263d6806f7e880c
rahulp <rahulp5875@gmail.com>
2026-06-04 19:15:01 +0530

    Add Flower service to Docker Compose for monitoring Celery tasks

--- app/core/tasks.py
+++ app/core/tasks.py
@@ -5,6 +5,7 @@ from django.core.mail import send_mail
 @shared_task
 def send_email(subject, message, sender_email, recipient_email):
     """Send an email using Django's send_mail helper."""
+    print("Sending email...")
     # Implement your email sending logic here. Example using send_mail:
     send_mail(
         subject,
