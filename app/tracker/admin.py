4e0a83309920f7ba9e931b9452e4f3add74f8ed5
rahulp <rahulp5875@gmail.com>
2026-06-07 13:36:15 +0530

    Code refactoring

--- app/tracker/admin.py
+++ app/tracker/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from tracker.models import (
+  BodyPart, MuscleGroup, Exercise,
+  Program, Workout, ProgramWorkout, WorkoutSets
+)
 
-# Register your models here.
+
+@admin.register(BodyPart)
+class BodyPartAdmin(admin.ModelAdmin):
+    """Admin page for body parts"""
+
+
+@admin.register(MuscleGroup)
+class MuscleGroupAdmin(admin.ModelAdmin):
+    """Admin page for muscle groups"""
+
+
+@admin.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    """Admin page for exercises"""
+    readonly_fields = ['created_at', 'updated_at']
+
+
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    """Admin page for workout programs"""
+    readonly_fields = ['created_at', 'updated_at']
+
+
+@admin.register(Workout)
+class WorkoutAdmin(admin.ModelAdmin):
+    """Admin page for workouts"""
+    readonly_fields = ['created_at']
+
+
+@admin.register(ProgramWorkout)
+class ProgramWorkoutAdmin(admin.ModelAdmin):
+    """Admin page for program workouts"""
+
+
+@admin.register(WorkoutSets)
+class WorkoutSetsAdmin(admin.ModelAdmin):
+    """Admin page for workout sets"""
+    readonly_fields = ['created_at']
--- app/tracker/models.py
+++ app/tracker/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 
+
 # Create your models here.
 class BodyPart(models.Model):
     """Model representing a body part."""
@@ -8,11 +9,16 @@ class BodyPart(models.Model):
 
     def __str__(self):
         return f"{self.name}"
-    
+
+
 class MuscleGroup(models.Model):
     """Model representing a muscle group."""
     name = models.CharField(max_length=100, unique=True)
-    body_parts = models.ForeignKey(BodyPart, related_name='muscle_groups', on_delete=models.CASCADE)
+    body_parts = models.ForeignKey(
+        BodyPart,
+        related_name='muscle_groups',
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return f"{self.name} ({self.body_parts.name})"
@@ -43,14 +49,25 @@ class Exercise(models.Model):
     execution = models.TextField(blank=True)
     comments = models.TextField(blank=True)
     difficulty = models.CharField(max_length=20, choices=DIFFICULTY_LEVELS)
-    muscle_groups = models.ManyToManyField(MuscleGroup, related_name='exercises')
-    equipment = models.CharField(max_length=50, choices=EQUIPMENT_TYPES, default='body_weight')
-    body_part = models.ManyToManyField(BodyPart, related_name='exercises', blank=True)
+    muscle_groups = models.ManyToManyField(
+        MuscleGroup,
+        related_name='exercises'
+    )
+    equipment = models.CharField(
+        max_length=50,
+        choices=EQUIPMENT_TYPES,
+        default='body_weight'
+    )
+    body_part = models.ManyToManyField(
+        BodyPart,
+        related_name='exercises',
+        blank=True
+    )
 
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     video_url = models.URLField(blank=True, null=True)
 
     def __str__(self):
@@ -61,18 +78,26 @@ class Program(models.Model):
     """Model representing a workout program."""
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='programs')
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='programs'
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     duration_in_weeks = models.PositiveIntegerField(default=4)
 
     def __str__(self):
         return f"{self.name}"
-    
+
 
 class Workout(models.Model):
     """Model representing a workout session."""
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='workouts')
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='workouts'
+    )
     name = models.CharField(max_length=100)
     date = models.DateTimeField()
     duration_min = models.PositiveIntegerField()
@@ -81,22 +106,40 @@ class Workout(models.Model):
 
     def __str__(self):
         return f"{self.name}"
-    
+
 
 class ProgramWorkout(models.Model):
     """Model representing the association between a workout and a program."""
-    program = models.ForeignKey(Program, on_delete=models.CASCADE, related_name='program_workouts')
-    workout = models.ForeignKey(Workout, on_delete=models.CASCADE, related_name='workout_programs')
+    program = models.ForeignKey(
+        Program,
+        on_delete=models.CASCADE,
+        related_name='program_workouts'
+    )
+    workout = models.ForeignKey(
+        Workout,
+        on_delete=models.CASCADE,
+        related_name='workout_programs'
+    )
     week_number = models.PositiveIntegerField()
     day_of_week = models.PositiveIntegerField()
-    
+
     def __str__(self):
-        return f"{self.program.name} - Week {self.week_number}, Day {self.day_of_week}"
-    
+        return f"{self.program.name} - Week {self.week_number},\
+            Day {self.day_of_week}"
+
+
 class WorkoutSets(models.Model):
     """Model representing the sets performed in a workout."""
-    workout = models.ForeignKey(Workout, on_delete=models.CASCADE, related_name='workout_sets')
-    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE, related_name='exercise_sets')
+    workout = models.ForeignKey(
+        Workout,
+        on_delete=models.CASCADE,
+        related_name='workout_sets'
+    )
+    exercise = models.ForeignKey(
+        Exercise,
+        on_delete=models.CASCADE,
+        related_name='exercise_sets'
+    )
     set_number = models.PositiveIntegerField(null=True, blank=True)
     repetitions = models.PositiveIntegerField(null=True, blank=True)
     weight_kg = models.FloatField(blank=True, null=True)
@@ -106,4 +149,5 @@ class WorkoutSets(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.workout.name} - {self.exercise.name} Set {self.set_number}"
+        return f"{self.workout.name} - {self.exercise.name}\
+            Set {self.set_number}"
